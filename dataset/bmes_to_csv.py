4eb81f6094a25feebac5d35b9352af63d0ec59c0
Lee Fingerhut <leefingerhut@gmail.com>
2021-07-22 14:19:54 +0300

    bmes to cvs of original dataset

--- dataset/bmes_to_csv.py
+++ dataset/bmes_to_csv.py
@@ -1,15 +1,22 @@
+import argparse
 import csv
 
+from pathlib import Path
 
-if __name__ == "__main__":
 
-    file_path = "morph_gold_train.bmes"
-    with open(file_path, 'r') as in_file:
+DATASETS_PATHS = (
+    Path("data/spmrl/gold"),
+    Path("data/ud/ab_annotators"),
+    Path("data/ud/gold"),
+)
+
+
+def bmes_to_cvs(source: Path, target: Path):
+    with open(source, 'r') as in_file:
         file = in_file.read()
         sentences = file.split("\n\n")
         sentences = [s.split("\n") for s in sentences]
         sentences = sentences[:-1]
-        splitted = []
         for i, sentence in enumerate(sentences):
             x = [s.split(" ") for s in sentence]
             z = [["", y[0], "", y[1]] for y in x]
@@ -17,7 +24,26 @@ if __name__ == "__main__":
             sentences[i] = z
 
     flat_list = [item for sublist in sentences for item in sublist]
-    with open('morph_gold_train.csv', 'w') as out_file:
+    with open(target, 'w') as out_file:
         writer = csv.writer(out_file)
         writer.writerow(('Sentence #', 'Word', 'POS', 'Tag'))
         writer.writerows(flat_list)
+
+
+if __name__ == "__main__":
+
+    output_dir = Path("cvs_data").expanduser()
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for dataset in DATASETS_PATHS:
+        source_dataset = dataset.expanduser()
+
+        output_dataset = output_dir.joinpath("/".join(dataset.parts[1:]))
+        output_dataset.mkdir(parents=True, exist_ok=True)
+
+        for file in dataset.iterdir():
+            if file.is_dir():
+                continue
+
+            output_file = output_dataset.joinpath(file.stem).with_suffix(".csv")
+            bmes_to_cvs(file, output_file)

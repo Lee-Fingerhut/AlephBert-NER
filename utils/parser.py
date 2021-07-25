54007b7db468caa8357a0745cb10114123f56106
Peleg Z <peleg122@gmail.com>
2021-07-25 22:27:09 +0300

    parameters changed to be dynamically changeable from the parameter file

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,7 @@
 import argparse
 import numpy as np
 import torch
-
-from datetime import datetime
+import utils.parameters as param
 from pathlib import Path
 
 
@@ -20,12 +19,12 @@ class Parser(object):
 
         general = parser.add_argument_group("general")
         general.add_argument("--seed", type=int, default=None, help="seed for reproducibility")
-        general.add_argument("--name", type=str, default=datetime.now().strftime("%d:%m_%H:%M:%S"),
+        general.add_argument("--name", type=str, default=param.todays_date_time,
                              help="name of directory for product")
 
         dataset = parser.add_argument_group("dataset")
         dataset.add_argument("--train-file", type=Path, required=True, help="path to train file")
-        dataset.add_argument("--max-seq-len", type=int, default=75, help="maximal sequence length")
+        dataset.add_argument("--max-seq-len", type=int, default=param.max_sequence_length, help="maximal sequence length")
 
         training = parser.add_argument_group("training")
         training.add_argument(
@@ -34,17 +33,17 @@ class Parser(object):
             default=False,
             help="set to finetune classifier rather than train entire model",
         )
-        training.add_argument("--num-epochs", type=int, default=25, help="number of epochs to train")
-        training.add_argument("--batch-size", type=int, default=32, help="batch size")
+        training.add_argument("--num-epochs", type=int, default=param.epochs, help="number of epochs to train")
+        training.add_argument("--batch-size", type=int, default=param.batch_size, help="batch size")
 
         optimizer = parser.add_argument_group("optimizer")
-        optimizer.add_argument("--learning-rate", type=float, default=3e-5, help="learning rate")
-        optimizer.add_argument("--optimizer-eps", type=float, default=1e-8, help="optimizer tolerance")
-        optimizer.add_argument("--weight-decay-rate", type=float, default=0.01, help="optimizer weight decay rate")
-        optimizer.add_argument("--max-grad-norm", type=float, default=1.0, help="maximal gradients norm")
+        optimizer.add_argument("--learning-rate", type=float, default=param.learning_rate, help="learning rate")
+        optimizer.add_argument("--optimizer-eps", type=float, default=param.optimizer_tolerance, help="optimizer tolerance")
+        optimizer.add_argument("--weight-decay-rate", type=float, default=param.optimizer_weight_decay_rate, help="optimizer weight decay rate")
+        optimizer.add_argument("--max-grad-norm", type=float, default=param.maximal_gradients_norm, help="maximal gradients norm")
 
         scheduler = parser.add_argument_group("scheduler")
-        scheduler.add_argument("--num-warmup-steps", type=int, default=0, help="scheduler warmup steps")
+        scheduler.add_argument("--num-warmup-steps", type=int, default=param.scheduler_warmup_steps, help="scheduler warmup steps")
 
         opts = parser.parse_args()
         set_seed(opts.seed)

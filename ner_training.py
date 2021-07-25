d79ccffaf009d0d64dbe7b81908bb2b827745b34
Lee Fingerhut <leefingerhut@gmail.com>
2021-07-26 00:49:00 +0300

    finetune

--- ner_training.py
+++ ner_training.py
@@ -85,9 +85,6 @@ if __name__ == "__main__":
     model = model.to(opts.device)
 
     if opts.finetune:
-        param_optimizer = list(model.classifier.named_parameters())
-        optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
-    else:
         param_optimizer = list(model.named_parameters())
         no_decay = ["bias", "gamma", "beta"]
         optimizer_grouped_parameters = [
@@ -97,6 +94,9 @@ if __name__ == "__main__":
             },
             {"params": [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], "weight_decay_rate": 0.0},
         ]
+    else:
+        param_optimizer = list(model.classifier.named_parameters())
+        optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
 
     optimizer = AdamW(optimizer_grouped_parameters, lr=opts.learning_rate, eps=opts.optimizer_eps)
 
